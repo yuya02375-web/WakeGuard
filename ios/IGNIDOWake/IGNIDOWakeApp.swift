ac540b8341daeba698514c7db941e1a831402950
yuya02375-web <yuya02375@gmail.com>
2026-09-07 22:27:39 +0900

    Wire multi timer and streak parity stores into iOS app

--- ios/IGNIDOWake/IGNIDOWakeApp.swift
+++ ios/IGNIDOWake/IGNIDOWakeApp.swift
@@ -4,9 +4,11 @@ import SwiftUI
 struct IGNIDOWakeApp: App {
     @StateObject private var alarmStore = AlarmStore()
     @StateObject private var timerStore = TimerStore()
+    @StateObject private var multiTimerStore = MultiTimerStore()
     @StateObject private var stopwatchStore = StopwatchStore()
     @StateObject private var worldClockStore = WorldClockStore()
     @StateObject private var streakStore = StreakStore()
+    @StateObject private var streakParityStore = StreakParityStore()
 
     init() {
         InitialState.migrate()
@@ -18,9 +20,11 @@ struct IGNIDOWakeApp: App {
             RootView()
                 .environmentObject(alarmStore)
                 .environmentObject(timerStore)
+                .environmentObject(multiTimerStore)
                 .environmentObject(stopwatchStore)
                 .environmentObject(worldClockStore)
                 .environmentObject(streakStore)
+                .environmentObject(streakParityStore)
                 .tint(IgnidoTheme.ember)
                 .preferredColorScheme(.dark)
                 .task { await alarmStore.bootstrap() }
