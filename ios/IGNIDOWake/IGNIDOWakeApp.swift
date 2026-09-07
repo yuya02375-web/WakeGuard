c2f9cf592996dc012d753efed587dcdad86b65f5
yuya02375-web <yuya02375@gmail.com>
2026-09-07 13:31:14 +0900

    Wire iOS feature stores into app

--- ios/IGNIDOWake/IGNIDOWakeApp.swift
+++ ios/IGNIDOWake/IGNIDOWakeApp.swift
@@ -3,11 +3,19 @@ import SwiftUI
 @main
 struct IGNIDOWakeApp: App {
     @StateObject private var alarmStore = AlarmStore()
+    @StateObject private var timerStore = TimerStore()
+    @StateObject private var stopwatchStore = StopwatchStore()
+    @StateObject private var worldClockStore = WorldClockStore()
+    @StateObject private var streakStore = StreakStore()
 
     var body: some Scene {
         WindowGroup {
             RootView()
                 .environmentObject(alarmStore)
+                .environmentObject(timerStore)
+                .environmentObject(stopwatchStore)
+                .environmentObject(worldClockStore)
+                .environmentObject(streakStore)
                 .preferredColorScheme(.dark)
                 .task { await alarmStore.bootstrap() }
         }
